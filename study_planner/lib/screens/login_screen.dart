481727b5212c7d9da08431cecaf23a923f014f65
ahnjb <159648234+jeongbin-0509@users.noreply.github.com>
2026-02-26 16:00:27 +0900

    로그인 -> 매인 화면 이동 기능 구현

--- study_planner/lib/screens/login_screen.dart
+++ study_planner/lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'main_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -10,11 +11,29 @@ class LoginScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
-          children: const [
-            TextField(decoration: InputDecoration(labelText: "이메일")),
-            TextField(
+          children: [
+            const TextField(decoration: InputDecoration(labelText: "이메일")),
+
+            const SizedBox(height: 10),
+
+            const TextField(
               decoration: InputDecoration(labelText: "비밀번호"),
               obscureText: true,
+            ),
+
+            const SizedBox(height: 30),
+
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MainScreen()),
+                  );
+                },
+                child: const Text("로그인"),
+              ),
             ),
           ],
         ),
---
+++ study_planner/lib/screens/planner_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PlannerScreen extends StatelessWidget {
+  const PlannerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("플래너 화면", style: TextStyle(fontSize: 24))),
+    );
+  }
+}
